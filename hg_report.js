47830432932079dd67f61200b33df5317e3f4891
Unknown <mwoods008@west-mec.org>
2019-03-05 13:20:17 -0700

    final commit, ready for review

--- hg_report.js
+++ hg_report.js
@@ -12,27 +12,27 @@
 	
 */
 
-//HTML code to display the information for Dance Off VII
+//display information 
 var gameReport = "<h1>" + itemTitle + "</h1> <h2> By:" + itemManufacturer + "</h2> <img src='hg_" + itemID + ".png' alt='" + itemID + "' id='gameImg'/> <table> <tr><th>Product ID</th><td>" + itemID + "</td></tr> <tr><th>List Price</th><td>" + itemPrice + "</td></tr> <tr><th>Platform</th><td>" + itemPlatform + "</td></tr> <tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr> <tr><th>Condition</th><td>" + itemCondition + "</td></tr> <tr><th>Release</th><td>" + itemRelease + "</td></tr> </table>" + itemSummary;
-// display gameReport in the first article element of index.html
+// display gameReport 
 document.getElementsByTagName("article")[0].innerHTML = gameReport;
 
 //declare ratingsSum and ratingsCount
 var ratingsSum = 0;
 var ratingsCount = ratings.length;
 
-//For each rating, it adds the raitng value to rating sum
+//adding ratings together
 for (var i = 0; i < ratings.length; i++) {
     ratingsSum += ratings[i];
 }
 
-//ratingsSum divided by the amount of ratings = the average
+//getting the average
 var ratingsAvg = ratingsSum / ratingsCount;
 
-//Heading 1 for ratingReport html code
+//Heading 
 var ratingReport = "<h1>Customer Reviews</h1> <h2>" + ratingsAvg + " out of 5 stars (" + ratingsCount + " reviews)</h2>";
 
-//For 3 reviews, a new div, title, author, rating date, star rating, and summary is added onto the html code
+//making the reviews and star ratings
 for (var i = 0; i <= 2; i++) {
     ratingReport += "<div class='review'>";
     ratingReport += "<h1>" + ratingTitles[i] + "</h1>";
@@ -44,5 +44,5 @@ for (var i = 0; i <= 2; i++) {
     }
     ratingReport += "</td></tr></table>" + ratingSummaries[i] + "</div>";
 }
-//insert ratingReport code into the first aside in index.html
+
 document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
